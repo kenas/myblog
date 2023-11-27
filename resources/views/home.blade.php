4634a815717508dfbf3d1e2d615baf9a80b43c00
Libor <libor.gess11@gmail.com>
2023-11-27 21:50:48 +0100

    home update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,22 +4,22 @@
     <div class="hero">
   
     </div>
-    
-    <div class="content">
-        <article class="articles">
-            @foreach($allArticles as $article)
+    <div class="container">
+        <div class="content">
+            <article class="articles">
+                @foreach($allArticles as $article)
 
-                <h2>{{ $article->title }}</h2>
-                <p>{{ $article->content }}</p>
-                {{ $article->category->name }}
+                    <h2>{{ $article->title }}</h2>
+                    <p>{{ $article->content }}</p>
+                    {{ $article->category->name }}
 
-            @endforeach
-        </article>
+                @endforeach
+            </article>
 
-            <div class="hot-articles">
-                <h3>HOT POSTS</h3>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni eaque minima eum error, totam porro laudantium nemo commodi fugit quidem facere aspernatur repellendus consequuntur modi hic quasi itaque suscipit esse!
-            </div>
+                <div class="hot-articles">
+                    <h3>HOT POSTS</h3>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni eaque minima eum error, totam porro laudantium nemo commodi fugit quidem facere aspernatur repellendus consequuntur modi hic quasi itaque suscipit esse!
+                </div>
+        </div>
     </div>
-
 @endsection
